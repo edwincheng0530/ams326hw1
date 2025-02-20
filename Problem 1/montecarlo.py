b0b21e46e3094465480f7daead93f5d332d04a0a
Edwin Cheng <edwin.cheng@stonybrook.edu>
2025-02-19 21:49:52 -0500

    implement montecarlo

--- Problem 1/montecarlo.py
+++ Problem 1/montecarlo.py
@@ -1,17 +1,32 @@
 # PROBLEM DESCRIPTION
-# Implement Secan'ts method of finding roots given the function f(x) = e(-x^3) - x^4 - sinx.
-# The root x, should be satsify the inequality |x-r| < 0.5 x 10^-4, where r is the actual root, approximated
-# to be r = 0.641583. In the program, we need to specify the number of steps/iterations as well as the value
-# of x as well as f(x) at each iteration of the bisection method.  
+#
 
 # ALGORITHM DESCRIPTION
 # 
 
 
 import math
+import random
 
 root = 0.641583
 
-
 def f(x):
     return math.e ** (-x**3) - x**4 - math.sin(x)
+
+def montecarlo(min_value = 0.5, max_value = 0.75):
+    seed_value = 42
+    random.seed(seed_value)
+    iteration = 1
+    while True:
+        x = random.uniform(min_value, max_value)
+        x = round(x, 5)
+        y = f(x)
+        
+        print(str(iteration) + ")\tx: " + str(x) + "\ty: " + str(y), end="\n\n")
+        if abs(root-x) < (0.5*(10**(-4))) or y == 0:
+            break
+
+        iteration += 1
+    return 
+
+montecarlo()
